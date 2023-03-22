3b8b7ce5e33f17ccedffedc71e8cdf9668d60680
tsertic <tsertic5@gmail.com>
2023-03-22 10:53:18 +0100

    added metadata to pages

--- pages/roi/index.tsx
+++ pages/roi/index.tsx
@@ -1,11 +1,23 @@
+import { PageHead } from "@/components/layout/Head/Head";
 import { UnderConstruction } from "@/components/layout/UnderConstruction/UnderConstruction";
+import { ROIPageContent } from "@/STATIC_DATA/STATICPAGECONTENT";
 import React from "react";
 
 const RoiPage = () => {
+  const { title, description, metaTitle, imageUrl } = ROIPageContent.head;
+
   return (
-    <div>
-      <UnderConstruction />
-    </div>
+    <>
+      <PageHead
+        title={title}
+        description={description}
+        metaTitle={metaTitle}
+        imageUrl={imageUrl}
+      />
+      <div>
+        <UnderConstruction />
+      </div>
+    </>
   );
 };
 
